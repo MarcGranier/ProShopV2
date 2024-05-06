074859a54beea38fcda8a343c0e6830edd204ba9
Marc Granier <mgranier@comcast.net>
2024-05-06 13:58:58 -0400

    Remote broke/Ready for Production

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import checkObjectId from '../middleware/checkObjectId.js';
+const router = express.Router();
+import {
+	getProducts,
+	getProductById,
+	createProduct,
+	updateProduct,
+	deleteProduct,
+	createProductReview,
+	getTopProducts,
+} from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.get('/top', getTopProducts);
+router
+	.route('/:id')
+	.get(checkObjectId, getProductById)
+	.put(protect, admin, checkObjectId, updateProduct)
+	.delete(protect, admin, checkObjectId, deleteProduct);
+router.route('/:id/reviews').post(protect, checkObjectId, createProductReview);
+
+export default router; // Export the router
